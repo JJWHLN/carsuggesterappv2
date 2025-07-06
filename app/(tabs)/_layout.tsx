a8859c6a08db6ee13ac6e5930d4b7d48b216b827
JJWHLN <151460263+JJWHLN@users.noreply.github.com>
2025-07-06 18:47:01 +0100

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Search, Car, TrendingUp, FileText, User } from 'lucide-react-native';
+import { Chrome as Home, Search, Car, TrendingUp, FileText, User } from 'lucide-react-native';
 import { Spacing, Typography } from '@/constants/Colors';
 import { useThemeColors } from '@/hooks/useTheme';
 
